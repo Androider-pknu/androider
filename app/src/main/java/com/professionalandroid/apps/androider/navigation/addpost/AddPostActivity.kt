cac4b7edbc4304cf17167b143e10556dc1e61b9c
seonghwan <ksh970516@pukyong.ac.kr>
2020-07-15 21:57:36 +0900

    feat: Create select store layout

--- app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/AddPostActivity.kt
+++ app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/AddPostActivity.kt
@@ -1,13 +1,18 @@
 package com.professionalandroid.apps.androider.navigation.addpost
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.professionalandroid.apps.androider.R
+import kotlinx.android.synthetic.main.activity_add_post.*
 
 class AddPostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
+        btn_add_place.setOnClickListener {
+            startActivity(Intent(this, SelectStoreActivity::class.java))
+        }
     }
 }
