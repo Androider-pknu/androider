b789ce85b0032f4636f2942cbae0cecc66b542ce
seonghwan <ksh970516@pukyong.ac.kr>
2020-07-19 23:35:03 +0900

    feat: Implement country picker

---
+++ app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/PickCountryDialog.kt
@@ -0,0 +1,54 @@
+package com.professionalandroid.apps.androider.navigation.addpost
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.professionalandroid.apps.androider.R
+import kotlinx.android.synthetic.main.dialog_searchaddress_countrypicker.*
+
+class PickCountryDialog : BottomSheetDialogFragment() {
+    internal lateinit var listener: NoticeDialogListener
+
+    interface NoticeDialogListener {
+        fun onDialogCompleteClick(value: String)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        listener = context as? NoticeDialogListener
+            ?: throw ClassCastException((context.toString() + " must implement NoticeDialogListener"))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_searchaddress_countrypicker, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        initNumberPicker()
+        btn_countrypicker_pickcountry.setOnClickListener {
+            val returnValue =
+                resources.getStringArray(R.array.country_list)[numberpicker_pickcountrydialog.value]
+            listener.onDialogCompleteClick(returnValue)
+            dismiss()
+        }
+    }
+
+    private fun initNumberPicker() {
+        numberpicker_pickcountrydialog.minValue = 0
+        numberpicker_pickcountrydialog.maxValue = 3
+        numberpicker_pickcountrydialog.displayedValues =
+            resources.getStringArray(R.array.country_list)
+        numberpicker_pickcountrydialog.value = 3
+        numberpicker_pickcountrydialog.wrapSelectorWheel = false
+    }
+}
--- app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/SearchAddressActivity.kt
+++ app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/SearchAddressActivity.kt
@@ -1,13 +1,23 @@
 package com.professionalandroid.apps.androider.navigation.addpost
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.professionalandroid.apps.androider.R
+import kotlinx.android.synthetic.main.activity_searchaddress.*
 
-class SearchAddressActivity : AppCompatActivity() {
+class SearchAddressActivity : AppCompatActivity(), PickCountryDialog.NoticeDialogListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searchaddress)
+
+        btn_searchaddress_choosecountry.setOnClickListener {
+            val countryPicker = PickCountryDialog()
+            countryPicker.show(supportFragmentManager, "country_picker")
+        }
+    }
+
+    override fun onDialogCompleteClick(value: String) {
+        btn_searchaddress_choosecountry.text = value
     }
 }
