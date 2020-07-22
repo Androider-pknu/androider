03bd4cb16be070794329d480555ded3cd76a8c2e
seonghwan <ksh970516@pukyong.ac.kr>
2020-07-22 19:08:22 +0900

    feat: Apply custom radio button style

--- app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/addstore/ChooseBuildingActivity.kt
+++ app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/addstore/ChooseBuildingActivity.kt
@@ -1,8 +1,10 @@
 package com.professionalandroid.apps.androider.navigation.addpost.addstore
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.professionalandroid.apps.androider.R
 import com.professionalandroid.apps.androider.navigation.addpost.addstore.input.AddStoreActivity
 import kotlinx.android.synthetic.main.activity_choosebuilding.*
@@ -14,6 +16,14 @@ class ChooseBuildingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_choosebuilding)
         btn_sample.setOnClickListener {
             startActivity(Intent(this, AddStoreActivity::class.java))
+        }
+        btn_choosebuilding_building.setOnClickListener {
+            btn_choosebuilding_building.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
+            btn_choosebuilding_location.setTextColor(ContextCompat.getColor(applicationContext, R.color.blue_default))
+        }
+        btn_choosebuilding_location.setOnClickListener {
+            btn_choosebuilding_building.setTextColor(ContextCompat.getColor(applicationContext, R.color.blue_default))
+            btn_choosebuilding_location.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
         }
     }
 }
