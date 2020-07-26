6decc2ecde25f1fdd6b00687a2d6f702204cdb73
seonghwan <ksh970516@pukyong.ac.kr>
2020-07-26 20:19:58 +0900

    feat: Implement choosing category as scroll view

--- app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/addstore/input/ChooseCategoryActivity.kt
+++ app/src/main/java/com/professionalandroid/apps/androider/navigation/addpost/addstore/input/ChooseCategoryActivity.kt
@@ -1,9 +1,19 @@
 package com.professionalandroid.apps.androider.navigation.addpost.addstore.input
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.marginStart
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.professionalandroid.apps.androider.R
 import kotlinx.android.synthetic.main.activity_choosecategory.*
+import kotlinx.android.synthetic.main.item_category.view.*
 
 class ChooseCategoryActivity : AppCompatActivity() {
 
@@ -13,6 +23,37 @@ class ChooseCategoryActivity : AppCompatActivity() {
 
         btn_choosecategory_back.setOnClickListener {
             onBackPressed()
+        }
+
+        inflateContent()
+    }
+
+    private fun inflateContent() {
+        val categoryList = resources.getStringArray(R.array.store_category)
+        val extraList = resources.getStringArray(R.array.extra_store_category)
+        val layout = layout_choosecategory_scrolllinear
+        val inflater = LayoutInflater.from(this)
+
+        val store = TextView(this)
+        store.text = "음식점"
+        store.textSize = 13f
+        layout.addView(store)
+
+        for (name in categoryList) {
+            val view = inflater.inflate(R.layout.item_category, layout, false)
+            view.textview_itemcategory_category.text = name
+            layout.addView(view)
+        }
+
+        val extra = TextView(this)
+        extra.text = "기타 장소"
+        extra.textSize = 13f
+        layout.addView(extra)
+
+        for (name in extraList) {
+            val view = inflater.inflate(R.layout.item_category, layout, false)
+            view.textview_itemcategory_category.text = name
+            layout.addView(view)
         }
     }
 }
