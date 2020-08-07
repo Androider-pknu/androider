14c594f68caea258ecae2a5bd352cac4d0d7c452
oaojjj <oxojjjj@gmail.com>
2020-08-08 00:00:11 +0900

    feat: store info ui update

---
+++ app/src/main/java/com/professionalandroid/apps/androider/common/StoreInfoFragment.kt
@@ -0,0 +1,17 @@
+package com.professionalandroid.apps.androider.common
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.professionalandroid.apps.androider.R
+
+class StoreInfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_store_info, container, false)
+    }
+}
